05adbd8aa53c0924520e1f8dde4d9a4d30d4394a
Gule <gulumawakuma70@gmail.com>
2024-10-31 16:45:45 +0300

    add react folder structure into a project

---
+++ change-point-analysis/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+     <div className="text-2xl text-green-950 uppercase">Change point analysis and statistical modeling</div>
+    </>
+  )
+}
+
+export default App
